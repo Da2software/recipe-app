da5365f7fc6a85696a0ff47a1c458fcf573bb462
puh <puh@birkman.com>
2024-05-24 16:23:32 -0600

    user_api added

--- recipes_api/core/schema.py
+++ recipes_api/core/schema.py
@@ -2,6 +2,7 @@ import graphene
 from core.utils import EnvManager
 from bson.objectid import ObjectId
 from database.db_clients import MongoDatabase
+import re
 
 ENV = EnvManager()
 client = MongoDatabase().get_connection()
@@ -14,15 +15,37 @@ class Recipe(graphene.ObjectType):
     ingredients = graphene.List(graphene.String)
     directions = graphene.List(graphene.String)
     NER = graphene.List(graphene.String)
+    author_id = graphene.Int(required=False)
+    category = graphene.String(required=False)
 
 
 class Query(graphene.ObjectType):
-    recipes = graphene.List(Recipe)
+    recipes = graphene.List(Recipe, id=graphene.ID(), title=graphene.String(),
+                            ingredients=graphene.List(graphene.String),
+                            author_id=graphene.Int(),
+                            categories=graphene.List(graphene.String))
 
-    @staticmethod
-    def resolve_recipes(*args, **kwargs):
+    def resolve_recipes(self, info, id=None, title=None, ingredients=None,
+                        autor_id=None, categories=None):
         mongo_recipes = db[ENV.get_env("MONGO_COLLECTION")]
-        recipes = mongo_recipes.find()
+
+        query = {}
+        if id:
+            query["_id"] = ObjectId(id)
+        if title:
+            query["title"] = {"$regex": title, "$options": "i"}
+        if autor_id:
+            query["author_id"] = autor_id
+        if categories:
+            query["category"] = {"$in": categories}
+        if ingredients:
+            regex_pattern = "|".join(ingredients)
+            query["ingredients"] = {
+                "$regex": regex_pattern,
+                "$options": "i"
+            }
+
+        recipes = mongo_recipes.find(query)
         res = []
         for recipe in recipes:
             res.append(Recipe(
---
+++ users_api/core/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from passlib.hash import bcrypt
+from sqlalchemy.orm import relationship
+
+from .db_manager import SQLiteORM
+
+Base = SQLiteORM().get_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    user_name = Column(String, unique=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    def set_password(self, password: str):
+        self.hashed_password = bcrypt.hash(password)
